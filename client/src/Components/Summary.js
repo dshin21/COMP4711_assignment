73816eaac510f5a66d9e1eb1859eddf1c7e22b2b
Daniel Shin <danielshin234@gmail.com>
2019-02-27 09:27:29 -0800

    http -> https

--- client/src/Components/Summary.js
+++ client/src/Components/Summary.js
@@ -24,7 +24,7 @@ class Summary extends Component {
   addplayerInfo = _ => {
     let name = this.state.playerName;
     let score = this.props.playerScore;
-    fetch(`http://comp4711assignment.herokuapp.com/players/add?name=${name}&score=${score}`)
+    fetch(`https://comp4711assignment.herokuapp.com/players/add?name=${name}&score=${score}`)
       .then(response => response.json())
       .catch(err => console.log(err));
   };
@@ -34,7 +34,7 @@ class Summary extends Component {
   };
 
   getPlayerInfo = () => {
-    fetch("http://comp4711assignment.herokuapp.com/players")
+    fetch("https://comp4711assignment.herokuapp.com/players")
       .then(response => response.json())
       .then(response => this.setState({ playerInfo: response.data }, () => {}))
       .catch(err => console.log(err));
