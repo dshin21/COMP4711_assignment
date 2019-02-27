cffa189350ddebe6e5e98f2378adac685918d001
Daniel Shin <danielshin234@gmail.com>
2019-02-26 20:31:50 -0800

    win/lose row,col,score,tiles done

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -8,162 +8,204 @@ class Board extends Component {
     super(props);
     this.state = {
       currentDeckSize: 7,
+      row: 3,
+      col: 3,
+      tiles: 4,
+
       deck: [],
       selectedCards: [],
       playerScore: 0, //TODO:store in DB
       playerName: "", //TODO:store in DB
-      trials: 1 //TODO:store in DB
+      trials: 1
     };
-
-    this.rotate = this.rotate.bind(this);
+    console.log("constructor");
     this.start();
   }
 
-  start() {
-    // this.reset();
-    //zero: wrong ans
-    //one:  correct ans
-    // this.setState({ isRotated: false });//TODO: temp
-    for (
-      let zero = 0, one = 0;
-      zero < this.state.currentDeckSize * this.state.currentDeckSize;
-      zero++
-    ) {
+  componentDidMount = () => {
+    console.log("componentDidMount");
+    console.log(this.state.deck);
+  };
+
+  start = () => {
+    console.log("start");
+
+    let deck = this.state.deck;
+    let row = this.state.row;
+    let col = this.state.col;
+    let tiles = this.state.tiles;
+    // console.log(row, col, tiles);
+
+    for (let regTile = 0, ansTile = 0; regTile < row * col; regTile++) {
       let tempCard = {
-        name: zero,
+        name: regTile,
         isAnswer: false
       };
-      if (one <= this.state.currentDeckSize) {
+
+      if (ansTile < tiles) {
         tempCard.isAnswer = true;
-        this.state.deck[zero] = tempCard;
-        one++;
+        deck[regTile] = tempCard;
+        ansTile++;
       } else {
         tempCard.isAnswer = false;
-        this.state.deck[zero] = tempCard;
+        deck[regTile] = tempCard;
       }
     }
-
     this.setState({ deck: this.randomizeCards(this.state.deck) });
-  }
+  };
 
-  randomizeCards(array) {
+  randomizeCards = array => {
     for (var i = array.length - 1; i > 0; i--) {
       var j = Math.floor(Math.random() * (i + 1));
       var temp = array[i];
       array[i] = array[j];
       array[j] = temp;
     }
-    console.log(array);
     return array;
-  }
+  };
 
   //array holding cards
   // if clicked, check if it is marked with "answer"
   // if true, remain flipped
   // if false, lose point -> reset -> prev level
-  handleClick(name, index) {
-    if (!this.state.selectedCards.some(e => e.name === name)) {
-      if (this.state.selectedCards.length >= this.state.currentDeckSize + 1) {
-        this.check();
-      } else {
-        let deck = this.state.deck;
-        let selectedCards = this.state.selectedCards;
+  handleClick = index => {
+    let selectedCards = this.state.selectedCards;
+    let deck = this.state.deck;
+    let tiles = this.state.tiles;
 
-        deck[index].isClicked = true;
-        selectedCards.push(deck[index]);
+    deck[index].isClicked = true;
 
-        this.setState({
+    if (selectedCards[selectedCards.length - 1] !== deck[index]) {
+      selectedCards.push(deck[index]);
+      // console.log(selectedCards);
+      this.setState(
+        {
           selectedCards,
           deck
-        });
-
-        if (
-          this.state.selectedCards.length ===
-          this.state.currentDeckSize + 1
-        ) {
-          this.check();
+        },
+        () => {
+          if (selectedCards.length === tiles) this.check();
         }
-      }
+      );
     }
-  }
+  };
 
-  check() {
+  existDuplicateSelection = () => {
+    let currentSelection = this.state.currentSelection;
+    if (this.state.selectedCards.some(e => e.name == currentSelection))
+      return true;
+    else return false;
+  };
+
+  check = () => {
     let selectedCards = this.state.selectedCards;
-    let playerScore = this.state.playerScore;
-    let currentDeckSize = this.state.currentDeckSize;
-
+    let isWrongAns = false;
     for (let i = 0; i < selectedCards.length; i++) {
       if (!selectedCards[i].isAnswer) {
-        //TODO: -1 score && restart the game -1 size if not 3
-        this.setState({
-          currentDeckSize:
-            this.state.currentDeckSize <= 3
-              ? 3
-              : this.state.currentDeckSize - 1,
-
-          //TODO:store in DB
-          playerScore: this.playerScore <= 0 ? 0 : this.playerScore - 1,
-
-          //TODO:store in DB
-          playerName: ""
-        });
-
-        this.start();
-        return;
+        isWrongAns = true;
+        this.setState(
+          () => {
+            return {
+              row: this.determineNewBoardState("lose")[0],
+              col: this.determineNewBoardState("lose")[1],
+              tiles: this.determineNewBoardState("lose")[2],
+              playerScore: this.determineNewBoardState("lose")[3]
+            };
+          },
+          () => {
+            this.start();
+          }
+        );
       }
     }
-
-    this.setState(
-      {
-        playerScore: playerScore + 1,
-        currentDeckSize: currentDeckSize + 1
-      },
-      () => {
-        console.log(this.state.playerScore);
-        setTimeout(() => {
+    console.log("checking");
+    if (!isWrongAns) {
+      this.setState(
+        () => {
+          return {
+            row: this.determineNewBoardState("win")[0],
+            col: this.determineNewBoardState("win")[1],
+            tiles: this.determineNewBoardState("win")[2],
+            playerScore: this.determineNewBoardState("win")[3]
+          };
+        },
+        () => {
           this.start();
-        }, 3000);
-      }
-    );
-  }
+        }
+      );
+    }
+  };
 
-  reset() {
+  determineNewBoardState = winOrLose => {
+    let row = this.state.row;
+    let col = this.state.col;
+    let tiles = this.state.tiles;
+    let playerScore = this.state.playerScore;
+    let res = [];
+
+    if (winOrLose === "win") {
+      if (tiles - col === 2) {
+        row++;
+        col++;
+        tiles = row;
+      } else {
+        tiles++;
+      }
+
+      if (playerScore <= 0) playerScore = 0;
+      else playerScore++;
+    }
+
+    if (winOrLose === "lose") {
+      if (tiles - col === 1) {
+        if (row === 3 && col === 3) {
+          console.log("hi");
+          tiles--;
+        }
+      } else {
+        row--;
+        col--;
+        tiles = row + 2;
+      }
+
+      if (playerScore <= 0) playerScore = 0;
+      else playerScore--;
+    }
+
+    res.push(row, col, tiles, playerScore);
+    return res;
+  };
+
+  componentDidUpdate = () => {
+    console.log(this.state.row, this.state.col, this.state.tiles);
+    console.log(this.state.deck);
+  };
+
+  reset = () => {
     let deck = this.state.deck;
     let selectedCards = this.state.selectedCards;
     for (let i = 0; i < deck.length; i++) deck[i] = null;
     for (let i = 0; i < selectedCards.length; i++) selectedCards[i] = null;
-  }
+  };
 
-  rotate() {
+  rotate = () => {
     if (!this.state.isRotated) {
       this.setState({
         isRotated: true
       });
     }
-  }
+  };
 
-  // determineContainerSize() {
-  //   let currentDeckSize = this.state.currentDeckSize;
-  //   switch (currentDeckSize) {
-  //     case 3:
-  //       return 3;
-  //     case 4:
-  //     case 5:
-  //     case 6:
-  //     case 7:
-  //   }
-  // }
-
-  render() {
+  render = () => {
     return (
       <div>
         <ScoreBoard
           playerScore={this.state.playerScore}
-          numTiles={this.state.currentDeckSize}
+          numTiles={this.state.tiles}
           trials={this.state.trials}
         />
         <div
-          className={`playground playground-${this.state.currentDeckSize} ${
+          className={`playground playground-${this.state.row} ${
             this.state.isRotated ? "start-rotate" : ""
           }`}
         >
@@ -174,14 +216,14 @@ class Board extends Component {
                 isClicked={currentCard.isClicked}
                 isAnswer={currentCard.isAnswer}
                 rotate={this.rotate}
-                click={() => this.handleClick(currentCard.name, index)}
+                click={() => this.handleClick(index)}
               />
             );
           })}
         </div>
       </div>
     );
-  }
+  };
 }
 
 export default Board;
--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,58 +1,56 @@
 import React, { Component } from "react";
 
 class Card extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isStart: true
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      isStart: true
+    };
+  }
 
-    clicked(currentCard) {
-        this.props.click(currentCard);
-    }
+  componentDidMount = () => {
+    setTimeout(() => {
+      this.setState({ isStart: false });
+      this.forceUpdate();
+      this.props.rotate();
+    }, 500);
+  };
 
-    componentDidMount() {
-        setTimeout(() => {
-            this.state.isStart = false;
-            this.forceUpdate();
-            this.props.rotate();
-        }, 3000);
-    }
+  handleClick = () => {
+    this.props.click(this.props.currentCard);
+  };
 
-    render() {
-        if (this.state.isStart) {
-            if (this.props.isAnswer) {
-                return (
-                    <div className={"card opened"}>
-                        <div className="front" />
-                        <div className="back" />
-                    </div>
-                );
-            } else {
-                return (
-                    <div className={"card"}>
-                        <div className="front" />
-                        <div className="back" />
-                    </div>
-                );
-            }
-        } else {
-            return (
-                <div
-                    className={
-                        "card " +
-                        (this.props.isStart || this.props.isClicked
-                            ? "opened"
-                            : "")
-                    }
-                    onClick={() => this.clicked(this.props.currentCard)}
-                >
-                    <div className="front" />
-                    <div className="back" />
-                </div>
-            );
-        }
+  render = () => {
+    if (this.state.isStart) {
+      if (this.props.isAnswer) {
+        return (
+          <div className={"card opened"}>
+            <div className="front" />
+            <div className="back" />
+          </div>
+        );
+      } else {
+        return (
+          <div className={"card"}>
+            <div className="front" />
+            <div className="back" />
+          </div>
+        );
+      }
+    } else {
+      return (
+        <div
+          className={
+            "card " +
+            (this.props.isStart || this.props.isClicked ? "opened" : "")
+          }
+          onClick={() => this.handleClick()}
+        >
+          <div className="front" />
+          <div className="back" />
+        </div>
+      );
     }
+  };
 }
 export default Card;
